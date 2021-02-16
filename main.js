c81e79541a594ae1a716b4323cb94cb466718a5b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-02-16 19:33:50 +0000

    added function casesCounter, refactored both update functions.

--- main.js
+++ main.js
@@ -10,7 +10,29 @@ form.addEventListener("submit", e => {
     updateCountry(country)
         .then(data => updateCountryStats(data))
         .catch(err => alert("No data for this country"));
-})
+});
+
+const casesCounter = () => {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 200;
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            const inc = target / speed;
+
+            if (count < target) {
+                counter.innerText = (count + inc).toFixed();
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target.toFixed().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+            }
+        };
+
+        updateCount();
+    });
+}
 
 const updateGlobalStats = async (data) => {
     const date = data.Date.toLocaleString();
@@ -19,25 +41,34 @@ const updateGlobalStats = async (data) => {
     <h1 class="header__h1">Covid 19 Stats! <img src="img/covid-19.svg" alt="covid image"></h1>
     <p class="header__paragraph-date">${date.slice(0, 10)}</p>
     <h2 class="header__h2">Whole world statistics:</h2>
-    <p class="header__paragraph">New cases: <span class="header__cases">${data.Global.NewConfirmed.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</span></p>
-    <p class="header__paragraph">Total cases: <span class="header__cases">${data.Global.TotalConfirmed.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</span></p>
-    <p class="header__paragraph">New deaths: <span class="header__deaths">${data.Global.NewDeaths.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</span></p>
-    <p class="header__paragraph">Total deaths: <span class="header__deaths">${data.Global.TotalDeaths.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</span></p>
-    <p class="header__paragraph">New recovered: <span class="header__recovered">${data.Global.NewRecovered.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</span></p>
-    <p class="header__paragraph">Total recovered: <span class="header__recovered">${data.Global.TotalRecovered.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</span></p>
+    <p class="header__paragraph">New cases: <span class="header__cases counter" data-target="${data.Global.NewConfirmed}">0</span></p>
+    <p class="header__paragraph">Total cases: <span class="header__cases counter" data-target="${data.Global.TotalConfirmed}">0</span></p>
+    <p class="header__paragraph">New deaths: <span class="header__deaths counter" data-target="${data.Global.NewDeaths}">0</span></p>
+    <p class="header__paragraph">Total deaths: <span class="header__deaths counter" data-target="${data.Global.TotalDeaths}">0</span></p>
+    <p class="header__paragraph">New recovered: <span class="header__recovered counter" data-target="${data.Global.NewRecovered}">0</span></p>
+    <p class="header__paragraph">Total recovered: <span class="header__recovered counter" data-target="${data.Global.TotalRecovered}">0</span></p>
     `
+    casesCounter();
 }
 
 const updateCountryStats = async (data) => {
-    
+
     countryContainer.innerHTML = `
     <h2 class="header__h2">Statistics for ${data !== undefined ? data.countryDetails.Country : "country"}:</h2>
-    <p class="form__paragraph">New cases: <span class="header__cases">${data !== undefined ? (data.countryDetails.Confirmed - data.countryDetails2.Confirmed).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "N/A"}</span></p>
-    <p class="form__paragraph">Total cases: <span class="header__cases">${data !== undefined ? data.countryDetails.Confirmed.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "N/A"}</span></p>
-    <p class="form__paragraph">New deaths: <span class="header__deaths">${data !== undefined ? (data.countryDetails.Deaths - data.countryDetails2.Deaths).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "N/A"}</span></p>
-    <p class="form__paragraph">Total deaths: <span class="header__deaths">${data !== undefined ? data.countryDetails.Deaths.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "N/A"}</span></p>
+    ${data !== undefined ?
+       `<p class="form__paragraph">New cases: <span class="header__cases counter" data-target="${data.countryDetails.Confirmed - data.countryDetails2.Confirmed}">0</span></p>
+        <p class="form__paragraph">Total cases: <span class="header__cases counter" data-target="${data.countryDetails.Confirmed}">0</span></p>
+        <p class="form__paragraph">New deaths: <span class="header__deaths counter" data-target="${data.countryDetails.Deaths - data.countryDetails2.Deaths}">0</span></p>
+        <p class="form__paragraph">Total deaths: <span class="header__deaths counter" data-target="${data.countryDetails.Deaths}">0</span></p>`
+    : 
+       `<p class="form__paragraph">New cases: <span class="header__cases">N/A</span></p>
+        <p class="form__paragraph">Total cases: <span class="header__cases">N/A</span></p>
+        <p class="form__paragraph">New deaths: <span class="header__deaths">N/A</span></p>
+        <p class="form__paragraph">Total deaths: <span class="header__deaths">N/A</span></p>`
+    }
+    
 `
-
+    casesCounter();
 }
 const updateCountry = async (country) => {
     const countryDetails = await getStats(country);
@@ -55,11 +86,12 @@ const getStats = async (country) => {
 
     if (country !== undefined) {
         const response = await fetch(base);
-        const data = await response.json();  
+        const data = await response.json();
         console.log(data)
-    return data;
-    
-}};
+        return data;
+
+    }
+};
 getStats()
     .then(data => updateCountryStats(data))
     .catch(err => console.log(err));
